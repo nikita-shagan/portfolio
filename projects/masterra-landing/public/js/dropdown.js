0431c4c765e750365159bed1f312f83124f4746b
Nikita Shagan <nikita.ydisk@yandex.ru>
2022-11-11 22:55:41 +0500

    projects delited

---
+++ projects/masterra-landing/public/js/dropdown.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("menu").classList.toggle("show-two");
+  document.getElementById("logo-menu").classList.toggle("change-image");
+}
+
