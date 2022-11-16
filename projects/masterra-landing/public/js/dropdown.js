18ba5f20c8f22a157bdd268f1863a5c88240aced
Nikita Shagan <nikita.ydisk@yandex.ru>
2022-11-16 20:20:05 +0500

    move portfolio to separate repos

--- projects/masterra-landing/public/js/dropdown.js
+++
@@ -1,6 +0,0 @@
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-  document.getElementById("menu").classList.toggle("show-two");
-  document.getElementById("logo-menu").classList.toggle("change-image");
-}
-
